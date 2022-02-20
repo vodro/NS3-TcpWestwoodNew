585cfc486fead17171840a894c4fbed609fd5da2
vodro <vdrkowshic@gmail.com>
2022-02-20 21:59:54 +0600

    tcp westwood new added but not incorporated

---
+++ scratch/_topology.cc
@@ -0,0 +1,380 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2018-20 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Aarti Nandagiri <aarti.nandagiri@gmail.com>
+ *          Vivek Jain <jain.vivek.anand@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// This program simulates the following topology:
+//
+//           1000 Mbps           10Mbps          1000 Mbps
+//  Sender -------------- R1 -------------- R2 -------------- Receiver
+//              5ms               10ms               5ms
+//
+// The link between R1 and R2 is a bottleneck link with 10 Mbps. All other
+// links are 1000 Mbps.
+//
+// This program runs by default for 100 seconds and creates a new directory
+// called 'bbr-results' in the ns-3 root directory. The program creates one
+// sub-directory called 'pcap' in 'bbr-results' directory (if pcap generation
+// is enabled) and three .dat files.
+//
+// (1) 'pcap' sub-directory contains six PCAP files:
+//     * bbr-0-0.pcap for the interface on Sender
+//     * bbr-1-0.pcap for the interface on Receiver
+//     * bbr-2-0.pcap for the first interface on R1
+//     * bbr-2-1.pcap for the second interface on R1
+//     * bbr-3-0.pcap for the first interface on R2
+//     * bbr-3-1.pcap for the second interface on R2
+// (2) cwnd.dat file contains congestion window trace for the sender node
+// (3) throughput.dat file contains sender side throughput trace
+// (4) queueSize.dat file contains queue length trace from the bottleneck link
+//
+// BBR algorithm enters PROBE_RTT phase in every 10 seconds. The congestion
+// window is fixed to 4 segments in this phase with a goal to achieve a better
+// estimate of minimum RTT (because queue at the bottleneck link tends to drain
+// when the congestion window is reduced to 4 segments).
+//
+// The congestion window and queue occupancy traces output by this program show
+// periodic drops every 10 seconds when BBR algorithm is in PROBE_RTT phase.
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/traffic-control-module.h"
+#include "ns3/flow-monitor-module.h"
+#include <stdlib.h>
+#include <time.h>
+
+#include <typeinfo>
+
+using namespace ns3;
+
+class _Edge
+{
+public:
+    _Edge() {}
+
+    _Edge(int first, int second, const std::string &dataRate, const std::string &delay) : first(first), second(second),
+                                                                                          dataRate(dataRate),
+                                                                                          delay(delay) {}
+
+    int first;
+    int second;
+    std::string dataRate;
+    std::string delay;
+};
+
+std::string dir;
+uint32_t *prev;
+Time prevTime = Seconds(0);
+
+// Calculate throughput
+static void
+TraceThroughput(Ptr<FlowMonitor> monitor)
+{
+    FlowMonitor::FlowStatsContainer stats = monitor->GetFlowStats();
+    Time curTime = Now();
+
+    int cnt = 0;
+    int total = 0;
+    for (auto itr = stats.begin(); itr != stats.end(); itr++)
+    {
+        // std::cout << cnt << std::endl;
+        std::ofstream thr(dir + "/throughput" + std::to_string(cnt + 1) + ".dat", std::ios::out | std::ios::app);
+        int diff = (itr->second.txBytes - prev[cnt]);
+        total += diff;
+        thr << curTime.GetSeconds() << " " << 8 * (itr->second.txBytes - prev[cnt]) / (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds())) << std::endl;
+        prev[cnt] = itr->second.txBytes;
+        cnt++;
+    }
+    std::ofstream thr(dir + "/throughput" + std::to_string(0) + ".dat", std::ios::out | std::ios::app);
+    thr << curTime.GetSeconds() << " " << 8 * total / (1000 * 1000 * (curTime.GetSeconds() - prevTime.GetSeconds())) << std::endl;
+
+    prevTime = curTime;
+
+    Simulator::Schedule(Seconds(0.2), &TraceThroughput, monitor);
+}
+
+// Check the queue size
+void CheckQueueSize(Ptr<QueueDisc> qd)
+{
+    uint32_t qsize = qd->GetCurrentSize().GetValue();
+    Simulator::Schedule(Seconds(0.2), &CheckQueueSize, qd);
+    std::ofstream q(dir + "/queueSize.dat", std::ios::out | std::ios::app);
+    q << Simulator::Now().GetSeconds() << " " << qsize << std::endl;
+    q.close();
+}
+
+// Trace congestion window
+static void CwndTracer(Ptr<OutputStreamWrapper> stream, uint32_t oldval, uint32_t newval)
+{
+    *stream->GetStream() << Simulator::Now().GetSeconds() << " " << newval / 1448.0 << std::endl;
+}
+
+void TraceCwnd(uint32_t nodeId, uint32_t socketId)
+{
+    AsciiTraceHelper ascii;
+    Ptr<OutputStreamWrapper> stream = ascii.CreateFileStream(dir + "/cwnd.dat");
+    Config::ConnectWithoutContext("/NodeList/" + std::to_string(nodeId) + "/$ns3::TcpL4Protocol/SocketList/" + std::to_string(socketId) + "/CongestionWindow", MakeBoundCallback(&CwndTracer, stream));
+}
+
+void shFlow(FlowMonitorHelper *flowmon, Ptr<FlowMonitor> monitor, std::string flow_file)
+{
+    AsciiTraceHelper ascii;
+    static Ptr<OutputStreamWrapper> flowStream = ascii.CreateFileStream(flow_file);
+
+    *flowStream->GetStream() << "Session DeliveryRatio LossRatio\n";
+    uint32_t SentPackets = 0;
+    uint32_t ReceivedPackets = 0;
+    uint32_t LostPackets = 0;
+    int j = 0;
+    Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon->GetClassifier());
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
+    for (std::map<FlowId, FlowMonitor::FlowStats>::const_iterator iter = stats.begin(); iter != stats.end(); ++iter)
+    {
+        Ipv4FlowClassifier::FiveTuple t = classifier->FindFlow(iter->first);
+        NS_LOG_UNCOND("\n");
+        NS_LOG_UNCOND("----Flow ID : " << iter->first);
+        NS_LOG_UNCOND(t.sourceAddress << " ===> " << t.destinationAddress);
+        NS_LOG_UNCOND("Sent Packets = " << iter->second.txPackets);
+        NS_LOG_UNCOND("Received Packets = " << iter->second.rxPackets);
+        NS_LOG_UNCOND("Lost Packets =" << iter->second.txPackets - iter->second.rxPackets);
+        NS_LOG_UNCOND("Packet delivery ratio = " << iter->second.rxPackets * 100.0 / iter->second.txPackets << "%");
+        NS_LOG_UNCOND("Packet loss ratio = " << (iter->second.txPackets - iter->second.rxPackets) * 100.0 / iter->second.txPackets << "%");
+
+        *flowStream->GetStream() << t.sourceAddress << "-" << t.destinationAddress << " " << iter->second.rxPackets * 100.0 / iter->second.txPackets << " " << (iter->second.txPackets - iter->second.rxPackets) * 100.0 / iter->second.txPackets << "\n";
+
+        SentPackets = SentPackets + (iter->second.txPackets);
+        ReceivedPackets = ReceivedPackets + (iter->second.rxPackets);
+        LostPackets = LostPackets + (iter->second.txPackets - iter->second.rxPackets);
+
+        j++;
+    }
+    NS_LOG_UNCOND("--------Total Results of the simulation----------" << std::endl);
+    NS_LOG_UNCOND("Total sent packets  =" << SentPackets);
+    NS_LOG_UNCOND("Total Received Packets =" << ReceivedPackets);
+    NS_LOG_UNCOND("Total Lost Packets =" << LostPackets);
+    NS_LOG_UNCOND("Packet Loss ratio =" << ((LostPackets * 100.0) / SentPackets) << "%");
+    NS_LOG_UNCOND("Packet delivery ratio =" << ((ReceivedPackets * 100.0) / SentPackets) << "%");
+    NS_LOG_UNCOND("Total Flod id " << j);
+}
+
+NS_LOG_COMPONENT_DEFINE("_topology");
+
+int main(int argc, char *argv[])
+{
+
+    LogComponentEnable("_topology", LOG_LEVEL_INFO);
+    // LogComponentEnable("TcpWestwoodNew", LOG_DEBUG);
+    // LogComponentEnable("TcpWestwood", LOG_LEVEL_ALL);
+    // LogComponentEnable("TcpSocketBase", LOG_DEBUG);
+
+    double error_p = 0.0;
+
+    // Naming the output directory using local system time
+    time_t rawtime;
+    struct tm *timeinfo;
+    char buffer[80];
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, sizeof(buffer), "%d-%m-%Y-%I-%M-%S", timeinfo);
+    std::string currentTime(buffer);
+
+    // std::string tcpTypeId = "TcpBbr";
+    //    std::string queueDisc = "FifoQueueDisc";
+    uint32_t delAckCount = 2;
+    //    bool bql = true;
+    bool enablePcap = false;
+    Time stopTime = Seconds(10);
+
+    CommandLine cmd(__FILE__);
+    std::string transport_prot = "TcpWestwoodNew";
+    cmd.AddValue("tcpTypeId", "Transport protocol to use: TcpNewReno, TcpBbr", transport_prot);
+    cmd.AddValue("delAckCount", "Delayed ACK count", delAckCount);
+    cmd.AddValue("enablePcap", "Enable/Disable pcap file generation", enablePcap);
+    cmd.AddValue("stopTime", "Stop time for applications / simulation time will be stopTime + 1", stopTime);
+    cmd.Parse(argc, argv);
+
+    transport_prot = std::string("ns3::") + transport_prot;
+    // NS_ABORT_MSG_UNLESS(TypeId::LookupByNameFailSafe(transport_prot, &tcpTid), "TypeId " << tcpTypeId << " not found");
+    Config::SetDefault("ns3::TcpL4Protocol::SocketType", TypeIdValue(TypeId::LookupByName(transport_prot)));
+
+    Config::SetDefault("ns3::TcpSocket::SndBufSize", UintegerValue(41943));
+    Config::SetDefault("ns3::TcpSocket::RcvBufSize", UintegerValue(62914));
+    Config::SetDefault("ns3::TcpSocket::InitialCwnd", UintegerValue(10));
+    Config::SetDefault("ns3::TcpSocket::DelAckCount", UintegerValue(delAckCount));
+    Config::SetDefault("ns3::TcpSocket::SegmentSize", UintegerValue(1448));
+
+    int numberOfNodes = 5;
+
+    std::vector<_Edge *> _edges;
+    //    std::vector<std::vector< Ipv4InterfaceContainer *> > interfaces(numberOfNodes);
+    _edges.push_back(new _Edge(0, 1, "100Mbps", "1ms"));
+    _edges.push_back(new _Edge(1, 2, "1Mbps", "35ms"));
+    _edges.push_back(new _Edge(2, 3, "100Mbps", "20ms"));
+    _edges.push_back(new _Edge(2, 4, "100Mbps", "1ms"));
+
+    NodeContainer allNodes;
+    allNodes.Create(numberOfNodes);
+
+    // Configure the error model
+    // Here we use RateErrorModel with packet error rate
+    Ptr<UniformRandomVariable> uv = CreateObject<UniformRandomVariable>();
+    uv->SetStream(50);
+    RateErrorModel error_model;
+    error_model.SetRandomVariable(uv);
+    error_model.SetUnit(RateErrorModel::ERROR_UNIT_PACKET);
+    error_model.SetRate(error_p);
+
+    PointToPointHelper tempLink;
+    // Assign IP addresses
+    Ipv4AddressHelper ipv4;
+    ipv4.SetBase("10.0.0.0", "255.255.255.0");
+
+    InternetStackHelper internet;
+    internet.Install(allNodes);
+    for (int _i = 0; _i < numberOfNodes - 1; _i++)
+    {
+        _Edge *edge = _edges.at(_i);
+        tempLink.SetDeviceAttribute("DataRate", StringValue(edge->dataRate));
+        tempLink.SetChannelAttribute("Delay", StringValue(edge->delay));
+
+        tempLink.SetDeviceAttribute("ReceiveErrorModel", PointerValue(&error_model));
+
+        NetDeviceContainer tempEdge = tempLink.Install(allNodes.Get(edge->first), allNodes.Get(edge->second));
+        ipv4.NewNetwork();
+        Ipv4InterfaceContainer tempConatainer = ipv4.Assign(tempEdge);
+    }
+
+    AsciiTraceHelper ascii;
+
+    // Populate routing tables
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables();
+    //    Ipv4GlobalRoutingHelper::PrintRoutingTableAllAt(Seconds(5), ascii.CreateFileStream("_topology_table"));
+
+    NS_LOG_INFO(" FLOW NOW");
+
+    //    for(int i = 0; i < numberOfNodes; i++) {
+    //        Ptr <Node> node = allNodes.Get(i); // Get pointer to ith node in container
+    //        Ptr <Ipv4> _ipv4 = node->GetObject<Ipv4>(); // Get Ipv4 instance of the node
+    //        NS_LOG_INFO(i << " : " << _ipv4->GetNInterfaces());
+    //        Ipv4Address addr = _ipv4->GetAddress(1, 0).GetLocal(); // Get Ipv4InterfaceAddress of xth interface.
+    //
+    ////        std::cout << addr << std::endl;
+    //        NS_LOG_INFO(addr);
+    //
+    //    }
+
+    int _number_of_flows = 5;
+
+    bool isFixed = false;
+    if (isFixed)
+    {
+        _number_of_flows = 3;
+    }
+    int _sources[] = {0, 0, 3};
+    int _sinks[] = {4, 3, 4};
+    srand(time(NULL));
+
+    prev = new uint32_t[2 * _number_of_flows];
+    for (int i = 0; i < 2 * _number_of_flows; i++)
+    {
+        prev[i] = 0;
+    }
+
+    int uniquePort = 44000;
+    for (int i = 0; i < _number_of_flows; i++)
+    {
+        int f = rand() % numberOfNodes;
+        int s = rand() % numberOfNodes;
+        if (isFixed)
+            f = _sources[i], s = _sinks[i];
+
+        if (s == f)
+        {
+            i--;
+            continue;
+        }
+        Ptr<Node> sourceNode = allNodes.Get(f);
+        Ptr<Node> sinkNode = allNodes.Get(s);
+
+        Ptr<Ipv4> _ipv4_source = sourceNode->GetObject<Ipv4>();                                                              // Get Ipv4 instance of the node
+                                                                                                                             //
+        Ipv4Address addr_source = _ipv4_source->GetAddress(1 + rand() % (_ipv4_source->GetNInterfaces() - 1), 0).GetLocal(); // Get Ipv4InterfaceAddress of xth interface.
+                                                                                                                             //
+
+        Ptr<Ipv4> _ipv4_sink = sinkNode->GetObject<Ipv4>();                                                            // Get Ipv4 instance of the node
+                                                                                                                       //
+        Ipv4Address addr_sink = _ipv4_sink->GetAddress(1 + rand() % (_ipv4_sink->GetNInterfaces() - 1), 0).GetLocal(); // Get Ipv4InterfaceAddress of xth interface.
+
+        // NS_LOG_INFO(" source : " << f << " => " << addr_source);
+        // NS_LOG_INFO(" sink   : " << s << " => " << addr_sink);
+        NS_LOG_INFO("( " << f << ", " << s << " ) : " << addr_source << " => " << addr_sink);
+
+        ++uniquePort;
+        BulkSendHelper source("ns3::TcpSocketFactory",
+                              InetSocketAddress(addr_sink, uniquePort));
+        source.SetAttribute("MaxBytes", UintegerValue(0));
+
+        ApplicationContainer sourceApps = source.Install(sourceNode);
+        sourceApps.Start(Seconds(0.0));
+        sourceApps.Stop(stopTime);
+
+        // Install application on the receiver
+        PacketSinkHelper sink("ns3::TcpSocketFactory",
+                              InetSocketAddress(Ipv4Address::GetAny(), uniquePort));
+        ApplicationContainer sinkApps = sink.Install(sinkNode);
+        sinkApps.Start(Seconds(0.0));
+        sinkApps.Stop(stopTime);
+    }
+    // Simulator::Schedule(Seconds(0.2), &TraceCwnd, 0, 0);
+
+    //    for(int )
+
+    //    NS_LOG_INFO(source- );
+    //    Address sourceLocalAddress(InetSocketAddress(source->Ge))
+
+    // Create a new directory to store the output of the program
+    dir = "_temp/" + std::string("now");
+    std::string dirToRem = "rm -R " + dir;
+    system(dirToRem.c_str());
+    std::string dirToSave = "mkdir -p " + dir;
+    if (system(dirToSave.c_str()) == -1)
+    {
+        exit(1);
+    }
+
+    // Check for dropped packets using Flow Monitor
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll();
+    Simulator::Schedule(Seconds(0 + 0.02), &TraceThroughput, monitor);
+
+    Simulator::Stop(stopTime + TimeStep(1));
+    Simulator::Run();
+
+    // flowmon.SerializeToXmlFile("mytest.flowmonitor", true, true);
+    Simulator::Destroy();
+
+    // shFlow(&flowmon, monitor, dir + "/tcpwestwood.data");
+
+    return 0;
+}
---
+++ src/internet/model/tcp-westwood-new.cc
@@ -0,0 +1,261 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2013 ResiliNets, ITTC, University of Kansas
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Siddharth Gangadhar <siddharth@ittc.ku.edu>,
+ *          Truc Anh N. Nguyen <annguyen@ittc.ku.edu>,
+ *          Greeshma Umapathi
+ *
+ * James P.G. Sterbenz <jpgs@ittc.ku.edu>, director
+ * ResiliNets Research Group  http://wiki.ittc.ku.edu/resilinets
+ * Information and Telecommunication Technology Center (ITTC)
+ * and Department of Electrical Engineering and Computer Science
+ * The University of Kansas Lawrence, KS USA.
+ *
+ * Work supported in part by NSF FIND (Future Internet Design) Program
+ * under grant CNS-0626918 (Postmodern Internet Architecture),
+ * NSF grant CNS-1050226 (Multilayer Network Resilience Analysis and Experimentation on GENI),
+ * US Department of Defense (DoD), and ITTC at The University of Kansas.
+ */
+
+#include "tcp-westwood-new.h"
+#include "ns3/log.h"
+#include "ns3/simulator.h"
+#include "rtt-estimator.h"
+#include "tcp-socket-base.h"
+
+NS_LOG_COMPONENT_DEFINE("TcpWestwoodNew");
+
+namespace ns3
+{
+
+    // need to understand this ?
+    NS_OBJECT_ENSURE_REGISTERED(TcpWestwoodNew);
+
+    TypeId
+    TcpWestwoodNew::GetTypeId(void)
+    {
+        static TypeId tid = TypeId("ns3::TcpWestwoodNew")
+                                .SetParent<TcpNewReno>()
+                                .SetGroupName("Internet")
+                                .AddConstructor<TcpWestwoodNew>()
+                                .AddAttribute("FilterType", "Use this to choose no filter or Tustin's approximation filter",
+                                              EnumValue(TcpWestwoodNew::TUSTIN), MakeEnumAccessor(&TcpWestwoodNew::m_fType),
+                                              MakeEnumChecker(TcpWestwoodNew::NONE, "None", TcpWestwoodNew::TUSTIN, "Tustin"))
+                                .AddTraceSource("EstimatedBW", "The estimated bandwidth",
+                                                MakeTraceSourceAccessor(&TcpWestwoodNew::m_currentBW),
+                                                "ns3::TracedValueCallback::Double");
+        return tid;
+    }
+
+    TcpWestwoodNew::TcpWestwoodNew(void) : TcpNewReno(),
+                                           m_currentBW(0),
+                                           m_lastSampleBW(0),
+                                           m_lastBW(0),
+                                           m_ackedSegments(0),
+                                           m_IsCount(false),
+                                           m_lastAck(0),
+                                           ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+                                           _bw_ratio(1)
+    ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+    {
+        NS_LOG_FUNCTION(this);
+    }
+
+    TcpWestwoodNew::TcpWestwoodNew(const TcpWestwoodNew &sock) : TcpNewReno(sock),
+                                                                 m_currentBW(sock.m_currentBW),
+                                                                 m_lastSampleBW(sock.m_lastSampleBW),
+                                                                 m_lastBW(sock.m_lastBW),
+                                                                 //  m_pType(sock.m_pType),
+                                                                 m_fType(sock.m_fType),
+                                                                 m_IsCount(sock.m_IsCount)
+    {
+        NS_LOG_DEBUG("TcpWestwoodNew::GetTypeId called !");
+
+        NS_LOG_FUNCTION(this);
+        NS_LOG_LOGIC("Invoked the copy constructor");
+    }
+
+    TcpWestwoodNew::~TcpWestwoodNew(void)
+    {
+    }
+
+    void
+    TcpWestwoodNew::PktsAcked(Ptr<TcpSocketState> tcb, uint32_t packetsAcked,
+                              const Time &rtt)
+    {
+        NS_LOG_FUNCTION(this << tcb << packetsAcked << rtt);
+
+        if (rtt.IsZero())
+        {
+            NS_LOG_WARN("RTT measured is zero!");
+            return;
+        }
+
+        m_ackedSegments += packetsAcked;
+        ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+        // NS_LOG_DEBUG("TcpWestwoodNew::PktsAcked " << m_ackedSegments);
+
+        ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+        // need some work here
+        //        if (m_pType == TcpWestwood::WESTWOOD)
+        //        {
+        EstimateBW(rtt, tcb);
+        //        }
+        //        else if (m_pType == TcpWestwood::WESTWOODPLUS)
+        //        {
+        //            if (!(rtt.IsZero () || m_IsCount))
+        //            {
+        //                m_IsCount = true;
+        //                m_bwEstimateEvent.Cancel ();
+        //                m_bwEstimateEvent = Simulator::Schedule (rtt, &TcpWestwood::EstimateBW,
+        //                                                         this, rtt, tcb);
+        //            }
+        //        }
+    }
+
+    void
+    TcpWestwoodNew::EstimateBW(const Time &rtt, Ptr<TcpSocketState> tcb)
+    {
+
+        NS_LOG_FUNCTION(this);
+
+        NS_ASSERT(!rtt.IsZero());
+
+        m_currentBW = m_ackedSegments * tcb->m_segmentSize / rtt.GetSeconds();
+
+        //        if (m_pType == TcpWestwood::WESTWOOD)
+        //        {
+        // NS_LOG_DEBUG("TcpWestWoodNew::EstimateBW before ==" << m_currentBW);
+
+        Time currentAck = Simulator::Now();
+        m_currentBW = m_ackedSegments * tcb->m_segmentSize / (currentAck - m_lastAck).GetSeconds();
+
+        // NS_LOG_DEBUG("TcpWestWoodNew::EstimateBW after ==" << m_currentBW << " rtt : " << rtt.GetSeconds() << " time diff : " << (currentAck - m_lastAck).GetSeconds());
+
+        m_lastAck = currentAck;
+        //        }
+        //        else if (m_pType == TcpWestwood::WESTWOODPLUS)
+        //        {
+        //            m_currentBW = m_ackedSegments * tcb->m_segmentSize / rtt.GetSeconds ();
+        //            m_IsCount = false;
+        //        }
+
+        m_ackedSegments = 0;
+        NS_LOG_LOGIC("Estimated BW: " << m_currentBW);
+
+        // Filter the BW sample
+
+        double alpha = 0.9;
+
+        if (m_fType == TcpWestwoodNew::NONE)
+        {
+        }
+        else if (m_fType == TcpWestwoodNew::TUSTIN)
+        {
+            double sample_bwe = m_currentBW;
+            m_currentBW = (alpha * m_lastBW) + ((1 - alpha) * ((sample_bwe + m_lastSampleBW) / 2));
+            ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+            // NS_LOG_DEBUG("TcpWestwoodNew::EstimateBW == "
+            //              << "_bw_ratio before: " << _bw_ratio);
+            // double _prev_ratio = _bw_ratio;
+            // NS_LOG_DEBUG("TcpWestwoodNew::EstimateBW == " << m_lastBW << " -> " << m_currentBW);
+            _bw_ratio = m_currentBW / m_lastBW;
+            // NS_LOG_DEBUG("TcpWestwoodNew::EstimateBW == "
+            //              << _prev_ratio << " -> " << _bw_ratio << " \n");
+
+            ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+            m_lastSampleBW = sample_bwe;
+            m_lastBW = m_currentBW;
+        }
+
+        NS_LOG_LOGIC("Estimated BW after filtering: " << m_currentBW);
+    }
+
+    uint32_t
+    TcpWestwoodNew::GetSsThresh(Ptr<const TcpSocketState> tcb,
+                                uint32_t bytesInFlight)
+    {
+        // std::cout << "TcpWestwoodNew:: GetSsThresh" << std::endl;
+
+        NS_UNUSED(bytesInFlight);
+        NS_LOG_LOGIC("CurrentBW: " << m_currentBW << " minRtt: " << tcb->m_minRtt << " ssthresh: " << m_currentBW * static_cast<double>(tcb->m_minRtt.GetSeconds()));
+
+        return std::max(2 * tcb->m_segmentSize,
+                        uint32_t(m_currentBW * static_cast<double>(tcb->m_minRtt.GetSeconds())));
+    }
+
+    /**
+     * \brief TcpWestwoodNew congestion avoidance
+     *
+     * During congestion avoidance, cwnd is incremented by our paper
+     * segment per round-trip time (RTT).
+     *
+     * \param tcb internal congestion state
+     * \param segmentsAcked count of segments acked
+     */
+    void
+    TcpWestwoodNew::CongestionAvoidance(Ptr<TcpSocketState> tcb, uint32_t segmentsAcked)
+    {
+        NS_LOG_FUNCTION(this << tcb << segmentsAcked);
+        // NS_LOG_DEBUG("TcpWestwoodNew::CongestionAvoidance");
+        if (segmentsAcked > 0)
+        {
+            double adder = static_cast<double>(tcb->m_segmentSize * tcb->m_segmentSize) / tcb->m_cWnd.Get();
+
+            adder = std::max(1.0, adder);
+
+            ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+            if (_bw_ratio >= 1.25)
+            {
+                adder *= 2;
+                NS_LOG_DEBUG("TcpWestwoodNew::CongestionAvoidance := "
+                             << "bandwidth ratio greater than 1.25 ");
+            }
+            else if (_bw_ratio >= 1)
+            {
+                adder *= 1;
+                NS_LOG_DEBUG("TcpWestwoodNew::CongestionAvoidance := "
+                             << " 1 <= bandwidth ratio < 1.5  ");
+            }
+            else
+            {
+                adder = 0;
+                NS_LOG_DEBUG("TcpWestwoodNew::CongestionAvoidance := "
+                             << " 0 < bandwidth ratio  ");
+            }
+            NS_LOG_DEBUG(Simulator::Now().GetSeconds() << "TcpWestwoodNew::CongestionAvoidance := adding : " << adder << " cWnd : " << tcb->m_cWnd.Get() << " b_ratio : " << _bw_ratio);
+
+            ////////////////////////////////////// vodro \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/
+
+            tcb->m_cWnd += static_cast<uint32_t>(adder);
+            NS_LOG_INFO("In CongAvoid, updated to cwnd " << tcb->m_cWnd << " ssthresh " << tcb->m_ssThresh);
+        }
+    }
+
+    Ptr<TcpCongestionOps>
+    TcpWestwoodNew::Fork()
+    {
+        return CreateObject<TcpWestwoodNew>(*this);
+    }
+
+} // namespace ns3
