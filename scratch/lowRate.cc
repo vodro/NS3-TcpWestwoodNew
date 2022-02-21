ba546f262359d0f3e1c8d48aec6f92736879a641
vodro <vdrkowshic@gmail.com>
2022-02-21 21:48:25 +0600

    started working on lowrate

---
+++ scratch/lowRate.cc
@@ -0,0 +1,19 @@
+
+#include <fstream>
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/internet-apps-module.h"
+#include "ns3/mobility-module.h"
+#include "ns3/spectrum-module.h"
+#include "ns3/propagation-module.h"
+#include "ns3/sixlowpan-module.h"
+#include "ns3/lr-wpan-module.h"
+#include "ns3/csma-module.h"
+
+using namespace ns3;
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
