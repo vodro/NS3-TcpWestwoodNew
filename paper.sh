f45dc5e749b9668b1a734af9ea753d519b1f68d0
vodro <vdrkowshic@gmail.com>
2022-02-22 13:53:13 +0600

    paper third accompanied

--- paper.sh
+++ paper.sh
@@ -3,7 +3,7 @@ prot=("TcpNewReno" "TcpWestwood" "TcpWestwoodNew")
 for i in "${prot[@]}"
 do
     echo $i
-    ./waf --run "_topology --transport_prot=$i"
+    ./waf --run "paper_third --transport_prot=$i"
 done
 
 cd plotting
